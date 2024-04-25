b2fc64517ef456222198c3cc4c1514de96806ab4
Szennnnnnnnnn <168090818+Szennnnnnnnnn@users.noreply.github.com>
2024-04-25 19:47:59 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cnqk08i6odq2.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "szenyap-employee"
+customregion = "us-east-1"
+
